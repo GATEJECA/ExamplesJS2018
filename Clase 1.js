4c2aa5eb469233402a13b73b5effc4d01b688d63
Carlos Garcia <carlos.garcia@ctplc.com>
2018-12-11 18:01:16 -0600

    Commit inicial

---
+++ Clase 1.js
@@ -0,0 +1,5 @@
+//Con lo siguiente puedo poner varios mensajes en un console agrupado, y el tipo de letra lo cambio con color
+console.group();
+console.log('%c This course is the best! ', 'color: #43b207');
+console.log('%c Siempre se puede aprender algo nuevo', 'color: red');
+console.groupEnd();
